504d7ddc0dee73cc93ddbe0dfa37beb5cb986717
w1nteRR <mykola796@gmail.com>
2021-03-20 13:15:56 +0200

    add nav links

---
+++ src/components/navigation/header/links.list.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react"
+import { NavLink } from "react-router-dom"
+
+import { useNavigationStyles } from "../navigation.styles"
+
+export const LinksList: FC = () => {
+
+  const styles = useNavigationStyles()
+  
+  const links = [
+    {
+      path: '/home',
+      name: 'Feed'
+    },
+    {
+      path: '/explore',
+      name: 'Explore'
+    },
+    {
+      path: '/library',
+      name: 'Library'
+    }
+  ]
+
+  return <>
+    {
+      links.map((link, index) => 
+        <NavLink 
+          key={index} 
+          to={link.path}
+          className={styles.navlink}
+          activeClassName={styles.navlinkActive}
+        >
+          {link.name}
+        </NavLink>
+      )
+    }
+  </>
+}

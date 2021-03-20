e9db8edae2a3f9aece824a64f0cac176bf504eef
w1nteRR <mykola796@gmail.com>
2021-03-20 13:24:46 +0200

    add user avatar

---
+++ src/components/common/user/user.avatar.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+
+interface IUserAvatarProps {
+  image?: string
+}
+
+const mockavatar = 'https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png'
+
+export const UserAvatar: FC<IUserAvatarProps> = ({
+  image
+}) => 
+  <div style={{ width: 30, height: 30 }}>
+    <img 
+      height='100%' 
+      width='100%' 
+      src={image || mockavatar} 
+      alt=""
+    />
+  </div>

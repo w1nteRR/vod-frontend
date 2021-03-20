28b7fdc323cd1463f6190e33607081dbf9bb4f2f
w1nteRR <mykola796@gmail.com>
2021-03-20 13:24:24 +0200

    add header

---
+++ src/components/navigation/header/header.bar.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react"
+import { AppBar, Box } from "@material-ui/core"
+
+import { useNavigationStyles } from "../navigation.styles"
+import { LinksList } from "./links.list"
+import { UserAvatar } from "../../common/user/user.avatar"
+
+export const HeaderNav: FC = () => {
+
+  const styles = useNavigationStyles()
+
+  return (
+    <AppBar position="static" className={styles.appbar}>
+      <Box>logo</Box>
+      <Box className={styles.linksBox}>
+        <LinksList />
+      </Box>
+      <UserAvatar />
+    </AppBar>
+  )
+}

445cc7a7e842f3ddc7bbabaa1505aa0e2521a138
w1nteRR <mykola796@gmail.com>
2021-04-03 16:46:36 +0300

    update theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,12 +3,22 @@ import { createMuiTheme } from "@material-ui/core"
 export const darkTheme = createMuiTheme({
   palette: {
     background: {
-      default: '#161616'
+      default: '#030303'
     }
   },
   typography: {
     allVariants: {
-      color: "#fff"
+      color: '#fafafa'
+    },
+    h1: {
+      fontWeight: 'lighter',
+      fontSize: 48
+    },
+    body1: {
+      fontSize: 11.5,
+      lineHeight: 2,
+      fontWeight: 600,
+      color: 'silver'
     }
   }
 })

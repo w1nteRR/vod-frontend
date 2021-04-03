62add03edc40bae209b33fb730b4d16f33151040
w1nteRR <mykola796@gmail.com>
2021-04-03 16:47:33 +0300

    rework list

--- src/components/common/list/styles.ts
+++ src/components/common/list/styles.ts
@@ -4,7 +4,17 @@ export const useListStyles = makeStyles({
   list: {
     display: 'flex',
     justifyContent: 'flex-start',
-    overflowX: 'scroll'
+    overflowX: 'scroll',
+    '&:hover': {
+      overflowX: 'scroll',
+      '&::-webkit-scrollbar-thumb': {
+        backgroundColor: 'gray'
+      }
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: 'unset',
+      borderRadius: 10
+    }
   },
   end: {
     height: 200,

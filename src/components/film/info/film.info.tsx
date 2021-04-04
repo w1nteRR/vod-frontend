aba46f0b404de09fc1a2d796e661b8f554cec635
w1nteRR <mykola796@gmail.com>
2021-04-04 23:13:44 +0300

    update film info

--- src/components/film/info/film.info.tsx
+++ src/components/film/info/film.info.tsx
@@ -3,20 +3,20 @@ import { Box, Typography } from "@material-ui/core"
 
 interface IFilmInfo {
   name: string
-  genr: Array<string>
+  genre: string
   plot: string
 }
 
 export const FilmInfo: FC<IFilmInfo> = ({
   name,
-  genr,
+  genre,
   plot
 }) => {
   return (
     <>
       <Box flex={1}>
         <Typography variant='h1'>{name}</Typography>
-        <Typography variant='overline'>{genr.map(i => i)}</Typography>
+        <Typography variant='overline'>{genre}</Typography>
       </Box>
       <Box flex={2} display='inherit' alignItems='center'>
         <Typography variant='body1'>{plot}</Typography>

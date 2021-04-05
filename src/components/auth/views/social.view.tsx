f23bd9085da349df5788b1e143587ae201a833fe
w1nteRR <mykola796@gmail.com>
2021-04-05 22:36:12 +0300

    add social view

---
+++ src/components/auth/views/social.view.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react"
+import { Box, Button, Typography } from "@material-ui/core"
+import { indigo } from '@material-ui/core/colors'
+import Icon from '@mdi/react'
+import { mdiFacebook, mdiGoogle } from '@mdi/js'
+
+export const SocialView: FC = () => {
+  return (
+    <Box display='flex' flexDirection='column' justifyContent='space-between' height={140}>
+      <Button 
+        startIcon={<Icon path={mdiGoogle} {...icon} />}
+        style={{
+          ...button, 
+          backgroundColor: indigo['A700']
+        }}
+      >
+        <Typography variant='button'>Sign in with Google</Typography>
+      </Button>
+      <Button 
+        startIcon={<Icon path={mdiFacebook} {...icon}/>}
+        style={{
+          ...button, 
+          backgroundColor: indigo[800]
+        }}
+      >
+        <Typography variant='button'>Sign in with Facebook</Typography>
+      </Button>
+    </Box>
+  )
+}
+
+const button = {
+  margin: '0 20px',
+  padding: '13px',
+  width: '100%'
+}
+
+const icon = {
+  size: .7,
+  color: '#fff'
+}

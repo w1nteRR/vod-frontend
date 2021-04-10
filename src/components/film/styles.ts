82042035d3c079572484802287382d215c87848e
w1nteRR <mykola796@gmail.com>
2021-04-11 00:15:43 +0300

    rework film styles

--- src/components/film/styles.ts
+++ src/components/film/styles.ts
@@ -3,14 +3,20 @@ import { makeStyles } from "@material-ui/styles"
 
 const HEIGHT = 400
 
-export const useFilmStyles = makeStyles<Theme, { poster: string }>(({
-  poster: ({ poster }) => ({
-    width: '100%',
-    height: HEIGHT,
-    
-    borderRadius: 5,
-
-    backgroundImage: `url(${poster})`,
+export const useFilmStyles = makeStyles<Theme, { wallpaper: string }>(({
+  main: {
+    margin: 80,
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  wallpaper: ({ wallpaper }) => ({   
+    height: '100vh', 
+    position: 'relative',
+    top: 0,
+    backgroundColor: 'silver',
+    backgroundImage: `url(${wallpaper})`,
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
     backgroundSize: 'cover'
@@ -20,6 +26,13 @@ export const useFilmStyles = makeStyles<Theme, { poster: string }>(({
     flexDirection: 'column',
     flex: 1,
     height: HEIGHT
+  },
+  gradient: {
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 36.98%, rgba(0, 0, 0, 0.37) 71.35%, #030303 100%)'
   },
   posterContainer: {
     minWidth: 300

ca974208d105f5371fd8c0c1e651ca39530f8ed8
w1nteRR <mykola796@gmail.com>
2021-04-13 00:11:57 +0300

    refactor

--- src/api/omdb.api.ts
+++ src/api/omdb.api.ts
@@ -5,12 +5,14 @@ export const omdbApi = () => {
     baseURL: 'http://www.omdbapi.com'
   })
 
+  const key = process.env.REACT_APP_OMDB_KEY
+
   return {
     film: async (name: string) => {
-      return await api.get(`?t=${name}&apikey=${process.env.REACT_APP_OMDB_KEY}`)
+      return await api.get(`?t=${name}&apikey=${key}`)
     },
     series: async (name: string, season: number) => {
-      return await api.get(`?t=${name}&Season=${season}&apikey=${process.env.REACT_APP_OMDB_KEY}`)
+      return await api.get(`?t=${name}&Season=${season}&apikey=${key}`)
     }
   }
 }

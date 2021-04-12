ef692e9a46805949ff9050041e6664fc83b6c974
w1nteRR <mykola796@gmail.com>
2021-04-13 00:04:19 +0300

    update user avatar

--- src/components/common/user/user.avatar.tsx
+++ src/components/common/user/user.avatar.tsx
@@ -2,14 +2,16 @@ import { FC } from "react";
 
 interface IUserAvatarProps {
   image?: string
+  onClick?: () => void
 }
 
 const mockavatar = 'https://i.pinimg.com/originals/7c/c7/a6/7cc7a630624d20f7797cb4c8e93c09c1.png'
 
 export const UserAvatar: FC<IUserAvatarProps> = ({
-  image
+  image,
+  onClick
 }) => 
-  <div style={{ width: 30, height: 30 }}>
+  <div style={{ width: 30, height: 30, cursor: 'pointer' }} onClick={onClick}>
     <img 
       height='100%' 
       width='100%' 

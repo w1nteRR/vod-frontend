332bd860c1be7dcc97b5246c3ee1db6773a1d7f3
w1nteRR <mykola796@gmail.com>
2021-04-13 00:03:31 +0300

    update header bar

--- src/components/navigation/header/header.bar.tsx
+++ src/components/navigation/header/header.bar.tsx
@@ -1,21 +1,24 @@
 import { FC } from "react"
 import { AppBar, Box } from "@material-ui/core"
+import { useHistory } from 'react-router-dom'
+
+import { UserAvatar } from "../../common/user/user.avatar"
 
 import { useNavigationStyles } from "../navigation.styles"
 import { LinksList } from "./links.list"
-import { UserAvatar } from "../../common/user/user.avatar"
 
 export const HeaderNav: FC = () => {
 
   const styles = useNavigationStyles()
-
+  const history = useHistory()
+  
   return (
-    <AppBar position="static" className={`${styles.appbar} backdrop`}>
+    <AppBar className={`${styles.appbar} backdrop`}>
       <Box>logo</Box>
       <Box className={styles.linksBox}>
         <LinksList />
       </Box>
-      <UserAvatar />
+      <UserAvatar onClick={() => history.push('/me')} />
     </AppBar>
   )
 }

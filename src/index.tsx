a6d56899e69041cf9491baa09ada4883e12dc066
w1nteRR <mykola796@gmail.com>
2021-04-15 00:03:36 +0300

    add auth0 config

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { CssBaseline, MuiThemeProvider } from '@material-ui/core'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 import { App } from './App'
 import store from './redux/store'
@@ -9,18 +10,27 @@ import store from './redux/store'
 import { darkTheme } from './theme'
 import './index.css'
 
+const domain = process.env.REACT_APP_DOMAIN
+const clientId = process.env.REACT_APP_CLIENT_ID
+
 ReactDOM.render(
   <BrowserRouter>
-    <Provider 
-      store={store}
+    <Auth0Provider
+      domain={domain!}
+      clientId={clientId!}
+      redirectUri={window.location.origin + '/login'}
     >
-      <MuiThemeProvider 
-        theme={darkTheme}
+      <Provider 
+        store={store}
       >
-        <CssBaseline />
-        <App />
-      </MuiThemeProvider>
-    </Provider>
+        <MuiThemeProvider 
+          theme={darkTheme}
+        >
+          <CssBaseline />
+          <App />
+        </MuiThemeProvider>
+      </Provider>
+    </Auth0Provider>
   </BrowserRouter>, 
   document.getElementById('root')
 )

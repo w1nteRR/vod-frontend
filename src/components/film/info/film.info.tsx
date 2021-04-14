622384e8259b085cad06fea417759c0e2e0dc830
w1nteRR <mykola796@gmail.com>
2021-04-15 00:05:40 +0300

    rework film info

--- src/components/film/info/film.info.tsx
+++ src/components/film/info/film.info.tsx
@@ -1,20 +1,28 @@
-import { FC } from "react"
+import { memo } from "react"
 import { Box, Typography } from "@material-ui/core"
+import Icon from '@mdi/react'
+import { mdiVideo4kBox, mdiSubtitles } from '@mdi/js'
+
 interface IFilmInfo {
-  name: string
   genre: string
   year: string
+  plot: string
 }
 
-export const FilmInfo: FC<IFilmInfo> = ({
-  name,
+export const Info = memo<IFilmInfo>(({
   genre,
-  year
+  year,
+  plot
 }) => {
   return (
-    <Box>
-      <Typography variant='h1'>{name}</Typography>
-      <Typography variant='overline'>{genre} • {year}</Typography>
-    </Box>    
+    <Box m={3} padding='0 10px' width='60%'>
+      <Typography variant='body1'>{plot}</Typography>
+      <Box display='flex' alignItems='center'>
+        <Typography variant='overline'>{genre} • {year}</Typography>
+        <Icon path={mdiVideo4kBox} size={.7} style={{ margin: '0 10px' }} />
+        <Icon path={mdiSubtitles} size={.7} style={{ margin: '0 10px' }} />
+      </Box>
+    </Box>   
   )
-}
+})
+

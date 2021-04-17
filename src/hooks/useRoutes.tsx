efa501d336c02ec7a0e0968b0c62505a4b5058ad
w1nteRR <mykola796@gmail.com>
2021-04-17 14:12:58 +0300

    rework auth flow

--- src/hooks/useRoutes.tsx
+++ src/hooks/useRoutes.tsx
@@ -2,22 +2,34 @@ import { Redirect, Route, Switch } from "react-router-dom"
 
 import { HeaderNav } from "../components/navigation/header/header.bar"
 
+import { Auth } from "../pages/auth/Auth"
 import { SignIn } from "../pages/auth/SignIn"
 import { Film } from "../pages/film/Film"
 import { Home } from "../pages/Home"
 import { Profile } from "../pages/profile/Profile"
+import { checkAuth } from "../redux/auth/thunks"
+import { useAppDispatch, useAppSelector } from "./useRedux"
 
 export const useRoutes = () => {
+  const { checking, isAuthenticated } = useAppSelector(state => state.auth)
+  const dispatch = useAppDispatch()
 
-  const auth = true
+  if(checking) {
 
-  if(auth) return (
+    dispatch(checkAuth())
+
+    return <p>checking auth</p>
+  }
+
+  if(isAuthenticated) return (
     <>
     <HeaderNav />
       <Switch>
         <Route path='/home' component={Home} />
         <Route path='/film/:id' component={Film} />
         <Route path='/me' component={Profile} />
+        
+        <Redirect to='/home' />
       </Switch>
     </>
   )
@@ -25,7 +37,9 @@ export const useRoutes = () => {
   return (
     <>
       <Switch>
-        <Route path='/signin' component={SignIn} />    
+        <Route path='/signin' component={SignIn} /> 
+        <Route path='/auth' component={Auth} />   
+      
         <Redirect to='/signin' />
       </Switch>
     </>

89119e99cfd42a603ba3df151a64df403efaf770
w1nteRR <mykola796@gmail.com>
2021-04-17 15:05:23 +0300

    add auth slice

--- src/redux/auth/slice.ts
+++ src/redux/auth/slice.ts
@@ -1,10 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit'
+
 import { initialState } from './types'
+import { checkAuth } from './thunks'
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {}
+  reducers: {
+    setAuth(state, action) {
+      state.isAuthenticated = action.payload.auth
+    }
+  },
+  extraReducers: builder => {
+    builder.addCase(checkAuth.fulfilled, (state, action) => {
+      state.checking = false
+      state.isAuthenticated = true
+      state.user = action.payload
+    })
+    builder.addCase(checkAuth.rejected, (state, action) => {
+      state.checking = false
+      state.isAuthenticated = false
+
+      console.log(action.error)
+    })
+  }
 })
+
+export const { setAuth } = authSlice.actions
 
 export default authSlice.reducer

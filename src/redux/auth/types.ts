782bbbcf194d7566cf0bde2f3a22af9e5598d888
w1nteRR <mykola796@gmail.com>
2021-04-17 15:06:22 +0300

    update store

--- src/redux/auth/types.ts
+++ src/redux/auth/types.ts
@@ -1,9 +1,11 @@
 interface AuthState {
   checking: boolean
+  isAuthenticated: boolean
   user: unknown //to do
 }
 
 export const initialState: AuthState = {
   checking: true,
+  isAuthenticated: false,
   user: {}
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import systemSlice from './system/slice'
+import authSlice from './auth/slice'
 
 const store = configureStore({
-  reducer: systemSlice
+  reducer: {
+    system: systemSlice,
+    auth: authSlice
+  }
 })
 
 export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 
 export default store
